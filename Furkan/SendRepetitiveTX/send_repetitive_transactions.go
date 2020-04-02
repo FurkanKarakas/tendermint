98c96aa147e3b264f1cdefa2f768af6ce0fb4e51
Furkan Karakas <fkarakasster@gmail.com>
2020-04-02 16:41:36 +0300

    config file changed new table created for stress testing

--- Furkan/SendRepetitiveTX/send_repetitive_transactions.go
+++ Furkan/SendRepetitiveTX/send_repetitive_transactions.go
@@ -23,7 +23,7 @@ func randSeq(n int) string {
 }
 
 // Sends transactions
-func sendTX(wg *sync.WaitGroup, portNr *string, TXSize *int, throughput *int) {
+func sendTX(wg *sync.WaitGroup, portNr *string, TXSize *int, myChan chan int) {
 	defer wg.Done() // Decrement by 1 after function returns.
 	resp, err := http.Get("http://127.0.0.1:" + *portNr + "/broadcast_tx_commit?tx=\"" + randSeq(*TXSize) + "\"")
 	if err != nil {
@@ -40,11 +40,18 @@ func sendTX(wg *sync.WaitGroup, portNr *string, TXSize *int, throughput *int) {
 		if strings.Contains(bodyString, "error") {
 			fmt.Println(bodyString)
 			if strings.Contains(bodyString, "timed out waiting for tx to be included in a block") {
-				*throughput++
+				myChan <- 1
 			}
 		} else {
-			*throughput++
+			myChan <- 1
 		}
+	}
+}
+
+// Counter counts the total valid transactions
+func Counter(throughput *int, myChan chan int) {
+	for {
+		*throughput += <-myChan
 	}
 }
 
@@ -59,11 +66,13 @@ func main() {
 
 	var wg sync.WaitGroup
 	throughput := 0
+	myChan := make(chan int)
+	go Counter(&throughput, myChan)
 	t1 := time.Now()
 
 	for i := 0; i < *TXNr; i++ {
 		wg.Add(1)
-		go sendTX(&wg, portNr, TXSize, &throughput)
+		go sendTX(&wg, portNr, TXSize, myChan)
 		fmt.Println("Sleeping", *TXTime, "milliseconds...", i+1)
 		time.Sleep(time.Duration(*TXTime) * time.Millisecond)
 	}
@@ -71,6 +80,8 @@ func main() {
 	wg.Wait()
 	t2 := time.Now()
 	timediff := t2.Sub(t1)
+
+	time.Sleep(1 * time.Millisecond)
 
 	throughputReal := float64(throughput) / float64(*TXNr) * 100
 	TXperS := float64(throughput) / timediff.Seconds()
