188697f04b747c60cede30b6da9fe9953a8ba5ba
Furkan Karakas <fkarakasster@gmail.com>
2020-04-23 17:19:15 +0300

    blocksize script updated, tex file added.

--- Furkan/BlockSize/block_size.go
+++ Furkan/BlockSize/block_size.go
@@ -6,21 +6,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
-// Foo stores the decoded HTTP response
-type Foo struct {
+// DecodedHTTPResponse stores the HTTP response from the server for the ABCI Info URL
+type DecodedHTTPResponse struct {
 	Jsonrpc string     `json:"jsonrpc"`
 	ID      int        `json:"id"`
 	Result  NestedJSON `json:"result"`
 }
 
-// NestedJSON stores the nested JSON object.
+// NestedJSON stores the nested JSON object. Relevant for the DecodedHTTPResponse struct.
 type NestedJSON struct {
 	Response NestedJSON2 `json:"response"`
 }
 
-// NestedJSON2 stores the 3rd layer.
+// NestedJSON2 stores the 3rd layer. Relevant for the NestedJSON struct.
 type NestedJSON2 struct {
 	Data             string `json:"data"`
 	Version          string `json:"version"`
@@ -29,7 +30,27 @@ type NestedJSON2 struct {
 	LastBlockAppHash string `json:"last_block_app_hash"`
 }
 
-// getJson decodes the HTTP JSON response.
+// BlockHeight stores the HTTP response of block height URL in a struct.
+type BlockHeight struct {
+	Result BlockHeight1 `json:"result"`
+}
+
+//BlockHeight1 is relevant, i.e. a nested JSON inside BlockHeight.
+type BlockHeight1 struct {
+	Block BlockHeight2 `json:"block"`
+}
+
+//BlockHeight2 is relevant, i.e. a nested JSON inside BlockHeight1.
+type BlockHeight2 struct {
+	Data BlockHeight3 `json:"data"`
+}
+
+//BlockHeight3 is relevant, i.e. a nested JSON inside BlockHeight2.
+type BlockHeight3 struct {
+	TXs []string `json:"txs"`
+}
+
+// getJSON decodes the HTTP JSON response.
 func getJSON(url string, target interface{}) {
 	r, err := http.Get(url)
 	if err != nil {
@@ -66,21 +87,41 @@ func getBlockSize(url string) int {
 	return len(bodyString)
 }
 
+// getBlockSizeinTX returns the size of the block in number of Transactions in it
+func getBlockSizeinTX(url string) int {
+	r, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	var target BlockHeight
+	jsonErr := json.Unmarshal(body, &target)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	return len(target.Result.Block.Data.TXs)
+
+}
+
 func main() {
 	portNr := flag.String("portNr", "26657", "Port numbers: 26657, 26660, 26662, and 26664. Default 26657")
-	height := flag.String("height", "", "Height of the block to look at. Default is the last height")
 	flag.Parse()
-	if *height == "" {
-		fmt.Println("No height given. Taking the last block height...")
-		foo1 := new(Foo)
-		getJSON("http://127.0.0.1:"+*portNr+"/abci_info?", foo1)
-		*height = foo1.Result.Response.LastBlockHeight
-		fmt.Println("Last block height is:", *height)
-	} else {
-		fmt.Println("Height argument is given:", *height)
+	ABCIInfo := new(DecodedHTTPResponse)
+	getJSON("http://127.0.0.1:"+*portNr+"/abci_info?", ABCIInfo)
+	lastHeight := ABCIInfo.Result.Response.LastBlockHeight
+	fmt.Println("Last block height is:", lastHeight)
+	lastHeightasInt, err := strconv.Atoi(lastHeight)
+	if err != nil {
+		panic(err)
 	}
 
-	size := getBlockSize("http://127.0.0.1:" + *portNr + "/block?height=" + *height)
-	fmt.Println("Size of the block is:", size, "bytes")
-
+	for i := 1; i <= lastHeightasInt; i++ {
+		size := getBlockSizeinTX("http://127.0.0.1:" + *portNr + "/block?height=" + strconv.Itoa(i))
+		fmt.Println("The number of transactions in the block with height", i, "is:", size)
+	}
 }
